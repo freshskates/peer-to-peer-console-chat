8f0985b3fac561c68a6f091352f45f87701a54ae
freshskates <business.robertcacho@gmail.com>
2021-03-15 00:13:51 -0700

    debugging init

--- client.py
+++ client.py
@@ -2,10 +2,29 @@
 # DatagramProtocol UDP
 from twisted.internet.protocol import DatagramProtocol
 from twisted.internet import reactor
+from random import randint
 
 # Make self.id UUID4
 
 
 class Client(DatagramProtocol):
+
     def __init__(self, host, port):
-        self.id = 2
+        if host == 'localhost':
+            host = '127.0.0.1'
+        self.id = host, port
+        self.address = None
+        print("Working on id: {}".format(self.id))
+
+    def socket_received(self, datagram, address):
+        print("{} : {}".format(address, datagram))
+
+    def send_message(self):
+        while True:
+            self.transport.write(input(":::").encode('utf-8'), self.address)
+
+
+if __name__ == '__main__':
+    port = randint(1000, 5000)
+    reactor.listenUDP(port, Client('localhost', port))
+    reactor.run()
